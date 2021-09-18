5767ccd67fc6d812b1127b182019bbbffe720d57
Luke Ehresman <luke@ehresman.org>
2021-09-18 13:29:36 -0400

    check

--- src/strings.ts
+++ src/strings.ts
@@ -12,49 +12,49 @@ export const MSG_addItem = {
 
 export const MSG_completedOn = {
   id: "52a19774-7f56-4365-814a-d0500e839e04",
-  description: "",
+  description: "The date that an item was completed.",
   defaultMessage: "Completed on {date}"
 };
 
 export const MSG_noteExample = {
   id: "134071e3-ac8c-4eeb-96d5-a7755e4098a0",
-  description: "",
+  description: "The label for an item in our shopping list.  This is the notes that they write to describe the item in the list.",
   defaultMessage: "Item"
 };
 
 export const MSG_cost = {
   id: "09262c8b-e632-42d0-a86d-b8ca80e73c2b",
-  description: "",
+  description: "This is the label that goes on top of where the user enters how much an item costs.",
   defaultMessage: "Cost"
 };
 
 export const MSG_language = {
   id: "a02f17a5-ee03-4957-8560-309f354cb897",
-  description: "",
+  description: "This is the label for a selector where the user can select their language.",
   defaultMessage: "Language"
 };
 
 export const MSG_currency = {
   id: "acf4fbcc-b571-4f73-8a8f-e3fb0ca7485b",
-  description: "",
+  description: "This is the label for a selector where the user can select their currency",
   defaultMessage: "Currency"
 };
 
 export const MSG_currencySymbolPosition = {
   id: "bd0ab78f-300a-4793-95ce-5c072e1bf13c",
-  description: "",
+  description: "This is the label for where a user can select how their currency symbol is displayed.  For example, in US the symbol is located before (e.g. $1.00) but some places in Canada show the symbol after the amount (e.g. 1.00$).",
   defaultMessage: "Symbol Position"
 };
 
 export const MSG_dateFormat = {
   id: "0676c26c-33ea-4444-9a4f-00cb79d29d78",
-  description: "",
+  description: "This is the label where a user can select how dates are formatted.",
   defaultMessage: "Date Format"
 };
 
 export const MSG_numberFormat = {
   id: "99f6e415-aed1-43f0-bfe1-a22e9a10584d",
-  description: "",
+  description: "This is the label where a user can select how numbers are formatted.",
   defaultMessage: "Number Format"
 };
 
